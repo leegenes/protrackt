147720f00180ca63462bcebdd4e769b938f659e0
Lee Haugen <haugenlee@leegenes.local>
2017-06-26 17:40:15 -0400

    edit models from examples; add TODO

--- models.py
+++ models.py
@@ -10,24 +10,29 @@ class Base(db.Model):
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
                                 onupdate=db.func.current_timestamp())
 
-class Topics(Base):
-    title = db.Column(db.String(500))
+class Users(Base):
+    username = db.Column(db.String(500))
+    user_first = db.Column(db.String(50))
+    user_last = db.Column(db.String(75))
+    user_preferred = db.Column(db.String(50))
+    user_email = db.Column(db.String(75))
+    user_password = db.Column(db.String(75))
 
     def __repr__(self):
-        return self.title
+        return self.user_preferred
 
-class Options(Base):
-    name = db.Column(db.String(200))
+# TODO: build other table models with link to Users
 
-class Polls(Base):
-    topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
-    option_id = db.Column(db.Integer, db.ForeignKey('options.id'))
-    vote_count = db.Column(db.Integer, default=0)
-    status = db.Column(db.Boolean)
-
-    topic = db.relationship('Topics', foreign_keys=[topic_id],
-            backref=db.backref('options', lazy='dynamic'))
-    option = db.relationship('Options', foreign_keys=[option_id])
-
-    def __repr__(self):
-        return self.option.name
+# exmaple for connecting tables
+# class Polls(Base):
+#     topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
+#     option_id = db.Column(db.Integer, db.ForeignKey('options.id'))
+#     vote_count = db.Column(db.Integer, default=0)
+#     status = db.Column(db.Boolean)
+#
+#     topic = db.relationship('Topics', foreign_keys=[topic_id],
+#             backref=db.backref('options', lazy='dynamic'))
+#     option = db.relationship('Options', foreign_keys=[option_id])
+#
+#     def __repr__(self):
+#         return self.option.name
