c2377f689b1c04ab6dd1489e1d29b1a971be165d
Lee Haugen <haugenlee@leegenes.local>
2017-06-27 16:45:21 -0400

    update teardown for accurate table drop

--- db_teardown.py
+++ db_teardown.py
@@ -1,7 +1,5 @@
 from app import db
 
-verify = input("Are you sure you want to drop all tables? [y/n]")
+verify = input("are you sure you want to drop all tables? [y/n]")
 if verify == 'y':
     db.drop_all()
-else:
-    print("Tables not deleted.")
