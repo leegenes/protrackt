2c987142d4889e19de8bd0afcc3c7946a461d26a
Lee Haugen <haugenlee@leegenes.local>
2017-06-27 12:49:14 -0400

    update add_user functionality

--- protrackt.py
+++ protrackt.py
@@ -1,10 +1,19 @@
+from flask import Flask, request, make_response, abort, jsonify
 from app import pt
-from app.models import db
-from sqlalchemy import create_engine
+from app.models import Base, User
+from sqlalchemy import create_engine, update
 
-@pt.route('/')
-def home():
-    return 'hello world'
+@pt.route('/api/v0/users/add_user', methods=['POST'])
+def add_user():
+    content = request.json
+    if not content:
+        abort(400)
+    user = User()
+    user.username = content['username']
+    user.email = content['email']
+    user.password = content['password']
+
+    return "New user, {}, added".format(user.username)
 
 if __name__ == '__main__':
     pt.run()
