8e73d9017fe75877b6038d3ff17e05af3ede7ddd
Lee Haugen <haugenlee@leegenes.local>
2017-06-27 21:08:42 -0400

    include add_org method, skeleton of get_user

--- protrackt.py
+++ protrackt.py
@@ -4,8 +4,18 @@ from app.models import Base, Users, UserDetail, Organization, \
     OrganizationDetail
 from uuid import uuid4
 
-@pt.route('/api/v0/users/add_user', methods=['POST'])
+### USER RELATED REQUESTS ###
+@app.route('/api/v0/users/add_user', methods=['POST'])
 def add_user():
+    """Add new user.
+
+    Receives
+    - username: string [required; unique]
+    - email: string [required; unique]
+    - password: string [required]
+
+    Creates new user with login credentials.
+    """
     content = request.json
     if not content:
         abort(400)
@@ -19,6 +29,50 @@ def add_user():
         return "New user, {}, added".format(user.username)
     except:
         abort(400, "User, {}, not added. Error on insert to db".format(user.username))
+
+@app.route('/api/v0/users/get_user/<id>', methods=['GET']):
+def get_user(id):
+    # TODO: return object of all
+    # {user: {organizations:
+    #   [{org:
+    #       {name:
+    #       projects:
+    #           [{name:,
+    #           start_date:,
+    #           end_date:,
+    #           skills: [],
+    #           desctiption:}]
+    #       start_date:
+    #       end_date:
+    #       type: ed/volunteer/work
+    #       address1:
+    #       ...}}]}}
+    pass
+
+### ORGANIZTION RELATED REQUESTS ###
+@app.route('/api/v0/users/<id>/add_org', methods=['POST'])
+def add_org(id):
+    """Adds new organization for given user.
+
+    Receives
+    - uuid: uuid4 [required]
+    - organization: string [required]
+    - type: string [required from education, work, volunteer]}
+    Creates new organization for user to build projects to.
+    """
+    content = request.json
+    if not content:
+        abort(400)
+    org = Organization()
+    org.name = content['name']
+    org.type = content['org_type']
+
+    try:
+        db.session.add(org)
+        return "New org, {}, added for uuid: {}".format(
+            org.name, content['uuid'])
+    except:
+        abort(400, )
 
 if __name__ == '__main__':
     app.run()
