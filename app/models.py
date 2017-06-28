e7c301ffd390b9283e3f5c1afb7dcebbb73591f6
Lee Haugen <haugenlee@leegenes.local>
2017-06-28 12:27:25 -0400

    update to project + skills focused structure

--- app/models.py
+++ app/models.py
@@ -1,5 +1,3 @@
-# TODO: write projects model; uncomment relationships to it
-
 from app import db
 from sqlalchemy.ext.declarative import declared_attr
 
@@ -55,10 +53,7 @@ class Experience(Base):
     description = db.Column(db.Text)
 
 class Organization(Experience):
-    __abstract__ = True
-
-    org_typ = db.Column(db.String(25), nullable=False)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(75), unique=True, nullable=False)
     address1 = db.Column(db.String(75))
     address2 = db.Column(db.String(25))
     city = db.Column(db.String(50))
@@ -66,34 +61,43 @@ class Organization(Experience):
     zipcode = db.Column(db.String(5))
     phone = db.Column(db.String(10))
     website = db.Column(db.String(100))
+    # projects = db.relationship('Project', backref='organization')
 
-class Company(Organization):
-    roles = db.relationship('Role', backref='company')
+class ProjectType(Base):
+    key = db.Column(db.String(3), nullable=False,
+        unique=True)
+    name = db.Column(db.String(25), nullable=False,
+        unique=True)
+
+class Project(Experience):
+    name = db.Column(db.String(50), nullable=False)
+    project_type = db.Column(db.String(3), db.ForeignKey('project_type.key'))
+
+class Skill(Base):
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+class ProjectSkill(Base):
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'),
+        nullable=False)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.id'),
+        nullable=False)
 
 class Role(Experience):
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'),
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'),
         nullable=False)
     title = db.Column(db.String(50), nullable=False)
-    # projects = db.relationship('Project', backref='role')
+    projects = db.relationship('Project', backref='role')
 
-class School(Organization):
-    focuses = db.relationship('Focus', backref='school')
-
-class Focus(Experience):
-    school_id = db.Column(db.Integer, db.ForeignKey('school.id'),
+class Education(Experience):
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'),
         nullable=False)
     title = db.Column(db.String(50), nullable=False)
     has_degree = db.Column(db.Boolean, nullable=False)
-    # projects = db.relationship('Project', backref='focus')
+    projects = db.relationship('Project', backref='education')
 
 class Course(Base):
-    focus_id = db.Column(db.Integer, db.ForeignKey('focus.id'),
+    ed_id = db.Column(db.Integer, db.ForeignKey('education.id'),
         nullable=False)
     title = db.Column(db.String(50), nullable=False)
     description = db.Column(db.Text)
-    # projects = db.relationship('Project', backref='course')
-
-class Skill(Base):
-    uuid = db.Column(db.String(32), db.ForeignKey('users.uuid'),
-        nullable=False)
-    name = db.Column(db.String(50), nullable=False)
+    projects = db.relationship('Project', backref='course')
