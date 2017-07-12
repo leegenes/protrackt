518e040953e9355ffb377fb535e23a228f5c3970
Lee Haugen <haugenlee@leegenes.local>
2017-07-12 11:49:37 -0400

    add and update endpoints for each model

--- app/protrackt_lib.py
+++ app/protrackt_lib.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 
 def convert_to_date(date_string):
-    if not date_string:
+    if not date_string or isinstance(date_string, datetime):
         return date_string
 
     return datetime.strptime(date_string, '%Y-%m-%d')
+
+def write_requested_attributes(db_object, request_content):
+    for key in request_content.keys():
+            if hasattr(db_object, key):
+                setattr(db_object, key, request_content[key])
+    return True
 
 if __name__ == "__main__":
     pass
--- app/requests.py
+++ app/requests.py
@@ -1,11 +1,10 @@
-from flask import request, make_response, abort, jsonify
+from flask import request, make_response, abort, jsonify, url_for
 from app import app, db, bcrypt
-from app.models import User, Organization, Project
+from app.models import User, Organization, Project, UserDetail, Role, Skill
 from uuid import uuid4
 import app.protrackt_lib as pl
 
 BASE_URL = '/api/v0'
-CREATE_URL = BASE_URL + '/users/<uuid>/create'
 
 ### USER RELATED REQUESTS ###
 @app.route(BASE_URL + '/users', methods=['POST'])
@@ -49,8 +48,8 @@ def get_user(uuid):
 
     Outputs user information.
     """
-
-    return User.query.filter_by(uuid).first()
+    u = User.query.filter_by(uuid=uuid).first()
+    return jsonify({'username': u.username}, 201)
 
 @app.route(BASE_URL + '/users/<uuid>', methods=['POST'])
 def update_user(uuid):
@@ -74,15 +73,25 @@ def update_user(uuid):
     if not content:
         abort(400)
 
+    user_detail = UserDetail.query.filter_by(uuid=uuid).first()
+    if not user_detail:
+        user_detail = UserDetail(**content)
+        user_detail.uuid = uuid
+        try:
+            db.session.add(user_detail)
+            db.session.commit()
+            return "Done!"
+        except:
+            abort(400)
     try:
-        user_detail = UserDetail.query.filter_by(uuid).first()
         user_detail.update(**content)
+        db.session.commit()
     except:
         abort(400)
 
 
 ### ORGANIZTION RELATED REQUESTS ###
-@app.route(CREATE_URL + '/org', methods=['POST'])
+@app.route(BASE_URL + '/users/<uuid>/org', methods=['POST'])
 def add_org(uuid):
     """Adds new organization for given user.
 
@@ -104,23 +113,59 @@ def add_org(uuid):
     """
 
     content = request.json
-    if not content or not content['name']:
+    if not content:
         abort(400)
 
-    content['start_date'] = pl.convert_to_date(content['start_date'])
-    content['end_date'] = pl.convert_to_date(content['end_date'])
-
     org = Organization(**content)
-
+    pl.convert_to_date(org.start_date)
+    pl.convert_to_date(org.end_date)
+    org.uuid = uuid
     try:
+        print("here3")
         db.session.add(org)
         db.session.commit()
         return "New org, {}, added for uuid: {}".format(
             org.name, uuid)
     except:
+        print("here4")
         abort(400)
 
-@app.route(CREATE_URL + '/role', methods=['POST'])
+@app.route(BASE_URL + '/users/<uuid>/org/<org_id>', methods=['POST'])
+def update_org(uuid, org_id):
+    """Updates organization for given user.
+
+    Receives
+    - uuid: uuid4 [required - in URL]
+    - org_id: integer [required - in URL]
+    - start_date: date [optional - cannot be null]
+    - end_date: date [optional]
+    - description: text [optional]
+    - name: string [required]
+    - address1: string [optional]
+    - address2: string [optional]
+    - city: string [optional]
+    - state: string(2) [optional]
+    - zipcode: string(5) [optional]
+    - phone: string(10) [optional]
+    - website: string(50) [optional]
+
+    Updates organization for with foreign key uuid.
+    """
+    content = request.json
+    # content['end_date'] = pl.convert_to_date(content['end_date']).date()
+    print(content)
+    org = Organization.query.filter_by(id=org_id).first()
+    print(org.id)
+    try:
+        pl.write_requested_attributes(org, content)
+        db.session.commit()
+        return "Success"
+    except:
+        print("failed")
+        abort(400)
+
+
+@app.route(BASE_URL + '/users/<uuid>/role', methods=['POST'])
 def add_role(uuid):
     """ Adds new role for given user.
 
@@ -128,7 +173,7 @@ def add_role(uuid):
     - uuid: uuid4 [required - in URL]
     - start_date: date [required]
     - end_date: date [optional]
-    - name: string [required]
+    - title: string [required]
     - description: text [optional]
     - org_id: integer [required]
 
@@ -136,23 +181,42 @@ def add_role(uuid):
     """
 
     content = request.json
-    if not content or not content['name'] or not content['org_id']:
+    if not content:
         abort(400)
 
     role = Role(**content)
-    role.start_date = pl.convert_to_date(role.start_date)
-    role.end_date = pl.convert_to_date(role.end_date)
+    role.start_date = pl.convert_to_date(role.start_date).date()
+    role.end_date = pl.convert_to_date(role.end_date).date()
     role.uuid = uuid
 
     try:
         db.session.add(role)
         db.session.commit()
         return "New role, {}, added to org, {}, for uuid: {}".format(
-            role.name, role.org_id, role.uuid)
+            role.title, role.org_id, role.uuid)
+    except:
+        abort(400)
+
+@app.route(BASE_URL + '/users/<uuid>/role/<role_id>', methods=['POST'])
+def update_role(uuid, role_id):
+    content = request.json
+
+    if not content:
+        abort(400)
+
+    role = Role.query.filter_by(id=role_id).first()
+    role.id = role_id
+
+    try:
+        pl.write_requested_attributes(role, content)
+        db.session.commit()
+        return "Successful POST"
+    except:
+        abort(400)
 
 
 
-@app.route(CREATE_URL + '/project', methods=['POST'])
+@app.route(BASE_URL + 'users/<uuid>/project', methods=['POST'])
 def add_project(uuid):
     """Adds new project for given user.
 
@@ -162,46 +226,49 @@ def add_project(uuid):
     - end_date: date [optional]
     - name: string [required]
     - description: text [optional]
-    - tags: array [optional]
     - role_id: integer [required]
 
     Create a new project associated to uuid and position/role/degree
     """
 
     content = request.json
-    if not content or not content['name']:
+    if not content:
         abort(400)
 
     content['start_date'] = pl.convert_to_date(content['start_date'])
 
-    project = Project({
-        'name': content['name'],
-        'start_date': pl.convert_to_date(content['start_date']),
-        'end_date': pl.convert_to_date(content['start_date']),
-        'description': content['description']
-        })
-
-    skills = []
-    for s in content['skills']:
-        skill = Skill({'name': s})
-
-        project_skill = ProjectSkill({
-            'project_id': project.id,
-            'skill_id': skill.id
-            })
-
-        skills.append({'skill': skill, 'project_skill': project_skill})
-
+    project = Project(**content)
+    pl.convert_to_date(project.start_date).date()
+    pl.convert_to_date(project.end_date).date()
     try:
         db.session.add(project)
-        for skill in skills:
-            db.session.add(skill['skill'])
-            db.session.add(skill['project_skill'])
         db.session.commit()
-        return "New project {} added for uuid {}".format(
-            project.name, uuid)
     except:
         abort(400)
+
+@app.route(BASE_URL + '/skill', methods=["POST"])
+def add_skill():
+    content = request.json
+
+    if not content or 'name' not in content:
+        abort(400)
+
+    if not Skill.query.filter_by(name=content['name']).first():
+        skill = Skill(**content)
+        try:
+            db.session.add(skill)
+            db.session.commit()
+            return "Skill added"
+        except:
+            abort(400)
+    else:
+        abort(400, "Skill already exists")
+
+@app.route(BASE_URL + '/skill/<skill_id>', methods=["GET"])
+def get_skill(skill_id):
+    skill = Skill.query.filter_by(id=skill_id).first()
+    return jsonify(skill_id=skill.id,
+                    skill=skill.name)
 
 
 
